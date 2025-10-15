b4ba04d58da4adf8a626ce3df32fe5570311a009
Gabriel Pauletti Egea <ga.paulettiegea@gmail.com>
2025-10-15 19:58:28 -0300

    refactor: Remove fixed off header and hiper links for navegation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "../icons/Logo";
 
 export default function Header() {
   return <>
-    <header className="w-screen h-max flex flex-col justify-center items-center py-4 fixed z-100 glassEffect">
+    <header className="w-screen h-max flex flex-col justify-center items-center py-4 z-100 absolute">
       <div className="w-[80%] h-full flex flex-col justify-between items-center gap-5">
         <div className="w-full h-max flex gap-10 max-sm:gap-2.5 items-center max-sm:flex-col-reverse max-sm:items-center">
           <figure className="flex gap-2.5 items-center">
@@ -24,12 +24,12 @@ export default function Header() {
             <Logo size="32px" />
             <h1 className="text-white font-bold text-xl">Tecno<span className="text-primary">Control</span></h1>
           </figure>
-          <ul className="text-white flex gap-5 text-sm max-sm:hidden">
+          {/* <ul className="text-white flex gap-5 text-sm max-sm:hidden">
             <li className="hover:text-primary cursor-pointer transition-all duration-300 font-semibold hover:scale-110">Home</li>
             <li className="hover:text-primary cursor-pointer transition-all duration-300 font-semibold hover:scale-110">Sobre nós</li>
             <li className="hover:text-primary cursor-pointer transition-all duration-300 font-semibold hover:scale-110">Serviços</li>
             <li className="hover:text-primary cursor-pointer transition-all duration-300 font-semibold hover:scale-110">Contate-nos</li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </header>
